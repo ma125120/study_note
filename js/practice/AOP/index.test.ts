4d135b74a41cad805d4cf0650d41b8ed18405e12
ma125120 <1418205177@qq.com>
2020-04-07 16:27:02 +0800

    fix: ts jest

---
+++ js/practice/AOP/index.test.ts
@@ -0,0 +1,7 @@
+import { UseInterceptor } from './index'
+
+import { User } from './example'
+
+test(`测试装饰器是否生效`, () => {
+  console.log(`测试`)
+})
--- js/practice/AOP/index.ts
+++ js/practice/AOP/index.ts
@@ -1,6 +1,7 @@
 import { plainToClass } from 'class-transformer'
 import { ClassType } from 'class-transformer/ClassTransformer'
 import 'reflect-metadata'
+
 function UseInterceptor(dataClass: ClassType<any>) {
   return (target: any, prop: string, desc: PropertyDescriptor) => {
     const oldValue = desc.value
